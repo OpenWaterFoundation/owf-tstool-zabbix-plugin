dfaaf67ccd0560c49d21631a1c3d2b3e77f67b46
Steve Malers <steve.malers@openwaterfoundation.org>
2025-04-12 10:09:08 -0600

    Update scripts to check plugin repositories.

--- build-util/git-check-product.sh
+++ build-util/git-check-product.sh
@@ -2,11 +2,29 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # This comment is required.
 # The above line ensures that the script can be run on Cygwin/Linux even with Windows CRNL.
 #
-# git-check-tstool - check the TSTool repositories for status
+# git-check-product [--onlyplugin] ...
+# - specify --onlyplugin to NOT prompt to check TSTool main repositories
+# - check the TSTool repositories for status
 # - this script calls the general git utilities script
 
 # Get the location where this script is located since it may have been run from any folder.
 scriptFolder=$(cd $(dirname "$0") && pwd)
+
+# The --onlyplugin parameter is used with the TSTool git-check-plugins script:
+# - possible command lines may include the following, do simple command line parsing:
+#    --onlyplugin
+#    or --debug --onlyplugin
+#
+# -if --onlyplugin is specified, don't prompt to check the main TSTool repositories.
+onlyplugin="false"
+if [ $# -ge 1 -a "$1" = "--onlyplugin" ]; then
+  onlyplugin="true"
+elif [ $# -ge 2 -a "$2" = "--onlyplugin" ]; then
+  onlyplugin="true"
+fi
+
+# Main plugin repository.
+mainRepo="owf-tstool-zabbix-plugin"
 
 # Git utilities folder is relative to the user's files in a standard development files location:
 # - determine based on location relative to the script folder
@@ -15,9 +33,18 @@ repoFolder=$(dirname ${scriptFolder})
 # Want the parent folder to the specific Git repository folder.
 gitReposFolder=$(dirname ${repoFolder})
 
-# Main plugin repository.
-mainRepo="owf-tstool-zabbix-plugin"
-
 # Run the general script.
-${scriptFolder}/git-util/git-check.sh -m "${mainRepo}" -g "${gitReposFolder}" $@
+${scriptFolder}/git-util/git-check.sh --mainrepo="${mainRepo}" --reposfolder="${gitReposFolder}" $@
 
+# Also check the main TSTool product repositories.
+if [ "${onlyplugin}" = "false" ]; then
+  tstoolBuildUtilFolder=${gitReposFolder}/cdss-app-tstool-main/build-util
+  echo ""
+  read -p "Also check the main TSTool product (y/N)? " answer
+  if [ "${answer}" = "y" -o "${answer}" = "Y" ]; then
+    ${tstoolBuildUtilFolder}/git-check-tstool.sh
+  fi
+  echo ""
+fi
+
+exit 0
